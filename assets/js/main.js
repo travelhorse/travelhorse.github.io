363e93a2cb02f283230b217e0214fe211d73e64c
diyanah08 <diyanah08@gmail.com>
2020-07-06 16:01:14 +0800

    finalised buttons and dropdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,28 +81,6 @@
     }
   });
 
-  $(document).on('click', '.mobile-nav-toggle', function(e) {
-    $('body').toggleClass('mobile-nav-active');
-    $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-    $('.mobile-nav-overly').toggle();
-  });
-
-  $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
-    e.preventDefault();
-    $(this).next().slideToggle(300);
-    $(this).parent().toggleClass('active');
-  });
-
-  $(document).click(function(e) {
-    var container = $(".mobile-nav, .mobile-nav-toggle");
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-      if ($('body').hasClass('mobile-nav-active')) {
-        $('body').removeClass('mobile-nav-active');
-        $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-        $('.mobile-nav-overly').fadeOut();
-      }
-    }
-
   // Navigation active state on scroll
   var nav_sections = $('section');
   var main_nav = $('.main-nav, .mobile-nav');
